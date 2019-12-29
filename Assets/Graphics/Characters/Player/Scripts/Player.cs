df619d252a9f2fb0d5e6e53323f59a1fdedbba7c
Matheus "Têco" Crivellari <matheus.acrivellari@gmail.com>
2019-12-29 03:54:26 -0300

    feat: add pentagram item

--- Assets/Graphics/Characters/Player/Scripts/Player.cs
+++ Assets/Graphics/Characters/Player/Scripts/Player.cs
@@ -2,21 +2,30 @@
 
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 public class Player : MonoBehaviour
 {
     [SerializeField]
     float speed = 10f;
+
+    [SerializeField]
+    int powerAmountToPossess = 5;
+
     Vector2 move = Vector2.zero;
     Rigidbody2D m_rb = null;
     SpriteRenderer m_sp = null;
+    Animator m_an = null;
+
 
     float lasth = 0f;
+    int power = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         m_rb = GetComponent<Rigidbody2D>();
         m_sp = GetComponent<SpriteRenderer>();
+        m_an = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -38,5 +47,21 @@ public class Player : MonoBehaviour
     {
         if (m_rb)
             m_rb.velocity = move;
+    }
+
+    void AddPower()
+    {
+        Power++;
+    }
+
+    int Power
+    {
+        get { return power; }
+        set
+        {
+            power = value;
+            if (power >= powerAmountToPossess)
+                m_an.SetBool("Empowered", true);
+        }
     }
 }
---
+++ Assets/Graphics/Items/Penta/Scripts/Penta.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+public class Penta : MonoBehaviour
+{
+    CircleCollider2D m_col = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_col = GetComponent<CircleCollider2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject go = collision.gameObject;
+        if (go.CompareTag("Player"))
+        {
+            go.SendMessage("AddPower", SendMessageOptions.DontRequireReceiver);
+            Destroy(gameObject);
+        }
+    }
+}
