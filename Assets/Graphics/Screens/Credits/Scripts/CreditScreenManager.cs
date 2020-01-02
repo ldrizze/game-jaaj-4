2197f5bd311c82ca53636003d7ac1eb145359c3f
Matheus "Têco" Crivellari <matheus.acrivellari@gmail.com>
2020-01-02 16:07:07 -0300

    feat: add credit screen

---
+++ Assets/Graphics/Screens/Credits/Scripts/CreditScreenManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditScreenManager : MonoBehaviour
+{
+    
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
