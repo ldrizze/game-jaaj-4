53cf1b0bfcf2b712324bea728623f5fc6449a01b
Matheus "Têco" Crivellari <matheus.acrivellari@gmail.com>
2020-01-02 15:06:11 -0300

    feat: add start screen

---
+++ Assets/Graphics/Start Screen/Scripts/StartScreenManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void ShowCredits()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit(0);
+    }
+}
