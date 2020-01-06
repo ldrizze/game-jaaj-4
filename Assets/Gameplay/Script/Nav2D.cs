c1d2d3576e4b9b550b7ff8907df25ec55323cfc5
Matheus "Têco" Crivellari <matheus.acrivellari@gmail.com>
2020-01-06 19:25:59 -0300

    feat: add script for nav2d

---
+++ Assets/Gameplay/Script/Nav2D.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class Nav2D : MonoBehaviour
+{
+    Tilemap tilemap = null;
+    int offsetX = 0;
+    int offsetY = 0;
+    int width = 0;
+    int height = 0;
+    float[,] graph;
+
+    void Start()
+    {
+        tilemap = GetComponent<Tilemap>();
+        SetupNav();
+    }
+
+    void Update()
+    {
+    }
+
+    void SetupNav()
+    {
+        if (!tilemap)
+            return;
+
+        offsetX = tilemap.cellBounds.x;
+        offsetY = tilemap.cellBounds.y;
+
+        width = tilemap.size.x;
+        height = tilemap.size.y;
+
+        graph = new float[height, width];
+
+        for (int y=0; y<height; y++)
+        {
+            for(int x=0; x<width; x++)
+            {
+                Tile t = (Tile) tilemap.GetTile(new Vector3Int(offsetX + x, offsetY + y, 0));
+
+                if (t)
+                    graph[y, x] = float.MaxValue;
+                else
+                    graph[y, x] = 1f;
+            }
+        }
+
+        Debug.Log(graph);
+    }
+}
---
+++ Assets/Gameplay/Script/Nav2DAgent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Nav2DAgent : MonoBehaviour
+{
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+}
